f192bb43cfde5d4af0ab32b78bc55d380942744a
PamePebess <pamepebess07@gmail.com>
2021-03-09 12:14:09 -0300

    Singers data + images

---
+++ src/data/themes/singers.js
@@ -0,0 +1,9 @@
+export default {
+    name: 'Singers',
+    items: [
+      { id: 'ElvisPresley', image: 'image/photos_singers/elvispresley.jpg'},
+      { id: 'RaffaellaCarra', image: 'image/photos_singers/raffaellacarra.jpg'},
+      { id: 'Raphael', image: 'image/photos_singers/raphael.jpg'},
+      { id: 'JuanGabriel', image: 'image/photos_singers/juangabriel.jpg'},
+    ],
+  };
