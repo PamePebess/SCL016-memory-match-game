858fb7e94c478506f2948518104f51d5d45ef87c
Camila Castro <ccastromn@gmail.com>
2021-03-09 12:14:47 -0300

    Fruits and TvShow Data + images

---
+++ src/data/themes/fruits.js
@@ -0,0 +1,9 @@
+export default {
+    name: 'Fruits',
+    items: [
+      { id: 'orange', image: 'image/photos_fruits/naranja.jpg'},
+      { id: 'kiwi', image: 'image/photos_fruits/kiwi.jpg' },
+      { id: 'pinneaple', image: "image/photos_fruits/piña.jpg" },
+      { id: 'banana', image: "image/photos_fruits/banana.jpg" },
+    ],
+  };
---
+++ src/data/themes/tv.js
@@ -0,0 +1,9 @@
+export default {
+    name: 'Tv Show',
+    items: [
+      { id: 'amaro', image: 'image/photos_tv/Amaro_GomezPablo.jpg'},
+      { id: 'cristian', image: 'image/photos_tv/Cristian_Sanchez.jpg' },
+      { id: 'donFrancisco', image: 'image/photos_tv/Don_Francisco.jpg' },
+      { id: 'tonka', image: 'image/photos_tv/tonka.jpg' },
+    ],
+  };
