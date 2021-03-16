36ad4ccd7d7ff437ac90a28b2082d73b1861ed1a
PamePebess <pamepebess07@gmail.com>
2021-03-16 12:06:40 -0300

    onecard revealed property

--- src/components/oneCard.js
+++ src/components/oneCard.js
@@ -1,10 +1,21 @@
 const createOneCardEl = (dataItems) =>{
+     //Función que muestre una carta al hacer click
+     const showCardClick =() =>{
+        dataItems.revealed = true;
+        oneCard.src = dataItems.image;
+        }
     //creamos una variable que guarda la creacion del elemento imagen
     const oneCard = document.createElement("img");
-    //le damos la ruta 
-    oneCard.src = dataItems.image;
+    //Crear addEventListener
+    oneCard.addEventListener("click", showCardClick)
+    //Crear condicion que nos muestra la carta dada vuelta
+    if (dataItems.revealed == false){
+        oneCard.src = 'image/backface.jpg';
+    } else {
+        oneCard.src = dataItems.image;
+    }
     //Nos retorna la carta creada
     return oneCard;
-}
+    }
 
 export default createOneCardEl;
